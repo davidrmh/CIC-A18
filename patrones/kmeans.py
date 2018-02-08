d34099313dfd0dd34d8ce826fbe3854e16fcde2e
David <davidricardo888@hotmail.com>
2018-02-08 13:59:52 -0600

    Add getClusters, recalculateCentroids,initializeClusters

--- patrones/kmeans.py
+++ patrones/kmeans.py
@@ -26,4 +26,39 @@ def normalize(dataSet):
     m=dataSet.shape[0]
     normDataSet=dataSet-tile(minVals,(m,1))
     normDataSet = normDataSet / tile(ranges,(m,1))
-    return array(normDataSet),ranges,minVals
+    normDataSet=array(normDataSet)
+    return normDataSet
+
+def getClusters(dataSet,centroids,classLabels,niter=500):
+    rows=dataSet.shape[0]
+    k=centroids.shape[0]
+
+    for iter in range(niter):
+        clusters = initializeClusters(k)
+        for row in range(rows):
+            distances=[]
+            for i in range(k):
+                distances.append(distEclud(dataSet[row],centroids[i]))
+            argMin=array(distances).argmin()
+            clusters[argMin]['classLabels'].append(classLabels[row])
+            clusters[argMin]['elements'].append(dataSet[row])
+        centroids=recalculateCentroids(clusters,k)
+    return clusters,centroids
+
+def recalculateCentroids(clusters,k):
+    n=array(clusters[0]['elements']).shape[1]
+    centroids=mat(zeros((k,n)))
+
+    #Calculates the mean per feature
+    for i in range(k):
+        centroids[i,:]=array(clusters[i]['elements']).mean(0)
+    return centroids
+
+def initializeClusters(k):
+    #Initializes the clusters
+    clusters=[]
+    for j in range(k):
+        clusters.append({})
+        clusters[j]['classLabels']=[]
+        clusters[j]['elements']=[]
+    return clusters
