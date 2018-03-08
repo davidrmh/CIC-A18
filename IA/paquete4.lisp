1740db14ac3c0fee733695a2ca1c55ed7b0de8af
David <davidricardo888@hotmail.com>
2018-03-07 19:45:36 -0600

    Add ejercicio 12

--- IA/paquete4.lisp
+++ IA/paquete4.lisp
@@ -135,3 +135,97 @@ cadena code
   resultado
   );let
 );defun
+
+;EJERCICIO 11
+;MULTIPLICACIÓN DE MATRICES VALIDANDO DIMENSIONES Y CON ARREGLOS
+
+;EJERCICIO 12
+
+(defun make-nodo(elemento)
+  "Agrega un nodo al árbol (método visto en clase)"
+  (cons (cons elemento nil) nil))
+
+(defun elemento(nodo)
+  "Regresa el elemento del nodo"
+  (first (first nodo)))
+
+(defun siguiente(nodo)
+  "Regresa los hermanos del nodo"
+  (rest nodo))
+
+(defun descendientes(nodo)
+  "Regresa los descendientes del nodo"
+  (rest (first nodo)))
+
+(defun está-elemento?(nodo elem)
+  "Busca un elemento en el árbol.
+  (Búsqueda en profundidad)
+  Si el elemento está presente regresa t
+  Si el elemento no está presente regresa nil
+  "
+  (cond
+    ((null nodo) nil)
+    ((equal (elemento nodo) elem) t) ;Si encuentra el elemento
+    (t (or (está-elemento? (descendientes nodo) elem) ;Busca en los hijo y hermanos
+      (está-elemento? (siguiente nodo) elem)))));defun
+
+(defun nivel-elemento(nodo elem &optional (nivel 0))
+  "Busca el nivel de un elemento en el árbol.
+  (Búsqueda en profundidad)
+  "
+  (cond
+    ((null nodo) nil)
+    ((equal (elemento nodo) elem) nivel) ;Si encuentra el elemento
+    (t (or (nivel-elemento (descendientes nodo) elem (1+ nivel)) ;Busca en los hijo y hermanos
+    (nivel-elemento (siguiente nodo) elem nivel)))));defun
+
+(defun inserta-nodo(padre elem)
+  "Inserta un nodo en un árbol binario
+  buscando recursivamente el padre en el cual pueda ser insertado
+  (el padre que tenga lugar para un hijo)
+  La búsqueda se hace por profundidad
+
+  Si no se pudo insertar regresa nil
+  "
+  (let ((hijo))
+    (cond
+      ((null padre) (return-from inserta-nodo nil)) ;El nodo es nil
+      ((null elem) (return-from inserta-nodo nil))) ;El elemento es nil
+    ;primero añade el nodo (si es posible)
+      (setq hijo (make-nodo elem))
+      (cond
+        ;El orden de las siguientes dos líneas tiene que ver
+        ;con la búsqueda por profundidad
+        ;Si intercambio el orden estaría realizando búsqueda por amplitud
+        ((eq (rest (first padre)) nil) (nconc (first padre) hijo));"Hijo"
+        ((eq (rest (rest (first padre))) nil) (nconc (rest (first padre)) hijo)) ;"Hermano"
+        (t (inserta-nodo (descendientes padre) elem)))));defun
+
+(defun btree(elem tree)
+  "Inserta un elemento en un árbol binario y regresa el nivel de
+  profundidad en el cual fue insertado
+
+  EJEMPLO DE USO
+  Primero crear la raíz
+  (defvar tree (make-nodo 'A))
+  Después añadir los elementos
+  (btree 'B tree)
+  (btree 'C tree)
+  (btree 'D tree) etc
+  "
+  (let ((flag-repetido nil) (flag-insert t) (nivel 0))
+    ;Revisa que el elemento no esté repetido
+    (setq flag-repetido (está-elemento? tree elem))
+    (when flag-repetido (format t "El elemento ya se encuentra")
+      (return-from btree nil))
+    ;Inserta el elemento
+    (setq flag-insert (inserta-nodo tree elem))
+    (when (not flag-insert)
+      (format t "No se pudo insertar el elemento, revisa que el padre o el elemento no sean nil")
+      (return-from btree nil))
+    ;Calcula el nivel
+    (setq nivel (nivel-elemento tree elem))
+    (format t "El elemento ~a se añadió en el nivel ~a~%" elem nivel)
+    nivel
+  );let
+);defun
