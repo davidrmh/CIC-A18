f10b12b87d58a100880a5e66b1dfc9a8d6fb504e
David <davidricardo888@hotmail.com>
2018-03-20 12:31:30 -0600

    Add crea-arreglo function, this function is needed in order to read UCI file chorales.lisp

--- Simbolico/dbscan.lisp
+++ Simbolico/dbscan.lisp
@@ -10,3 +10,32 @@
 ;;Si *vecinos* es null y además *memoria* contiene todas las observaciones
 ;; (sus índices), entonces terminar. En otro caso buscar el siguiente cluster
 ;; iniciando con una observación que no esté en *memoria*
+
+;;Detectar patrones centrales
+
+;;Patrón central en su e-vecindad tiene nmin elementos
+
+;;=========================================================
+;; Función para leer los datos y organizarlos en un arreglo
+;; Los datos originales vienen en un archivo .lisp
+;;=========================================================
+(defun crea-arreglo(ruta-archivo)
+"Función para leer el archivo de UCI
+ENTRADA
+ruta-archivo: cadena con la ruta del archivo de UCI (chorales.lisp)
+SALIDA:
+arreglo: una lista de listas conteniendo la información arreglada.
+"
+  (let ((linea nil) (arreglo nil) (archivo nil) (nobs nil))
+    (setq archivo (open ruta-archivo))
+    (loop for i from 1 to 100 do
+      (setq linea (rest (read archivo)))
+      (setq nobs (length linea));número de observaciones en la línea
+      ;modifica la observación j
+      (loop for j from 0 to (1- nobs) do
+        (loop for k from 0 to 5 do
+          (setf (nth k (nth j linea)) (second (nth k (nth j linea))) ));loop k
+        (setq arreglo (append arreglo (list (nth j linea)))));loop j
+    );loop i
+  (close archivo)
+  arreglo));defun
