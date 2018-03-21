2dcb2d5bb8cfb1f539ac7d78a527f4e889934c97
David <davidricardo888@hotmail.com>
2018-03-21 17:59:07 -0600

    Major changes to dbscan function and implementation fo main-dbscan

--- Simbolico/dbscan.lisp
+++ Simbolico/dbscan.lisp
@@ -39,6 +39,7 @@
 (defun reset-all()
   (setq *con-grupo* nil)
   (setq *centrales* nil)
+  (setq *centrales-permanentes* nil)
   (setq *vecinos* nil)
   (setq *tabla* nil)
   (setq *grupo* 1)
@@ -47,6 +48,7 @@
       1618 3676 2524 3121 1198 2055 2987 2440 3258 1679))
   (setq *ruido* nil)
   (setq *todas-obs* nil)
+  (setq *frontera* nil)
 );defun
 
 ;;=========================================================
@@ -194,13 +196,6 @@ lista:Una lista con los índices de los patrones centrales
 );defun
 
 ;;=======================================================
-;; Actualiza tabla asignando etiquetas
-;;=======================================================
-(defun actualiza-tabla(puntos etiqueta)
-
-);defun
-
-;;=======================================================
 ;; Función para encontrar los vecinos de un patrón
 ;;=======================================================
 (defun encuentra-vecinos(matriz renglon eps)
@@ -210,13 +205,14 @@ lista:Una lista con los índices de los patrones centrales
       (when (and (<= (get-element renglon col matriz) eps)
                  (not (member col *ignorar*)) (/= renglon col))
         (setq resultado (append resultado (list col)))))
+    resultado
   );let
 );defun
 
 ;;========================================================
 ;; Función para asignar grupos a vecinos
 ;;========================================================
-(defun asigna-grupos(vecinos)
+(defun asigna-grupo(vecinos)
   (loop for elem in vecinos do
     (if (not (member elem *con-grupo*))
       (setq *con-grupo* (append *con-grupo* (list elem))) )
@@ -240,29 +236,79 @@ lista:Una lista con los índices de los patrones centrales
   (let ((ind-cent nil) (ind-vec nil) (dim 0))
     (setq dim (array-dimension matriz 0))
     (setq *centrales* (encuentra-centrales matriz eps mu))
-    (setq *centrales-permanentes* (copy-seq *centrales*))
+    (setq *centrales-permanentes* (copy-seq *centrales*)) ;para referencia
    (loop until (null *centrales*) do
-     (setq ind-cent (pop *centrales*))
-     (when (not (member ind-cent *con-grupo*))
-       (push ind-cent *con-grupo*)
+
+     (setq ind-cent (pop *centrales*)) ;Elige un patrón central
+
+     (when (not (member ind-cent *con-grupo*)) ;Si no se ha clasificado
+       (push ind-cent *con-grupo*) ;Agrega a la lista de los que ya tienen un grupo
+
+       ;actualiza tabla
+       (setf (aref *tabla* ind-cent 2) :central) ;tipo de patrón
+       (setf (aref *tabla* ind-cent 3) *grupo*) ;grupo
+
+      ;obtiene sus vecinos
        (setq *vecinos* (encuentra-vecinos matriz ind-cent eps))
-       (asigna-grupos *vecinos*)
-       ;actualiza tabla falta determinar que tipo de punto son (centrales,frontera,ruido)
+      ;Estos vecinos son patrones frontera o centrales
+      ;por lo tanto tendrán un grupo
+       (asigna-grupo *vecinos*)
        (loop until (null *vecinos*) do
-         (setq ind-vec (pop *vecinos*))
-         (when (member ind-vec *centrales-permanentes*) ;cuando es directamente alcanzable
-           (loop for i from 0 to (1- dim) do
-             (when (and (<= (get-element ind-vec i matriz) eps) (not (member i *con-grupo*)))
-                (push i *vecinos*)
-                (push i *con-grupo*)
-              ;actualiza tabla falta determinar que tipo de punto son (centrales,frontera,ruido)
-             );when
-           );loop
+         (setq ind-vec (pop *vecinos*)) ;Elige un vecino
+
+         ;El vecino es un patrón frontera?
+         (when (not (member ind-vec *centrales-permanentes*))
+           (push ind-vec *frontera*)
+          ;actualiza tabla
+          (setf (aref *tabla* ind-vec 2) :frontera)
+          (setf (aref *tabla* ind-vec 3) *grupo*));when
+
+         ;El vecino es un patrón central?
+         (when (member ind-vec *centrales-permanentes*)
+          ;actualiza tabla
+          (setf (aref *tabla* ind-vec 2) :central)
+          (setf (aref *tabla* ind-vec 3) *grupo*)
+
+            ;Encuentra los patrones indirectamente alcanzables
+            ;desde el patrón central ind-cent utilizando como
+            ;patrón intermedio el patrón ind-vec
+            (loop for i from 0 to (1- dim) do
+              ;los puntos que cumplen las condiciones
+              ;son density-reachables (indirectamente alcanzables)
+              (when (and (<= (get-element ind-vec i matriz) eps) (not (member i *con-grupo*)))
+                  (push i *vecinos*)
+                  (push i *con-grupo*)
+              );when
+            );loop
          );when
-        );loop
-     );when
+
+       );loop *vecinos*
+     );when patrón central no se ha clasificado
     (incf *grupo*)
-   );loop
+   );loop *centrales
    (setq *ruido* (set-difference *todas-obs* *con-grupo*))
   );let
+);defun
+
+
+;;=============================================================
+;; Función principal
+;;=============================================================
+
+(defun main-dbscan(ruta-datos eps mu)
+  (let ((matriz nil) (datos nil) (clases nil))
+
+    (reset-all);reinicia variables globales
+
+    (setq datos (crea-lista ruta-datos));lee datos
+    (setq clases (first (first datos)));obtiene la clase a la que pertenece cada observación
+    (setq datos (first (first (rest datos)))) ;datos numéricos
+    (setq matriz (matriz-distancias datos));Obtiene la matriz de distancias
+
+    ;Inicializa variables globales
+    ;(setq *ignorar* (genera-aleatorios 25 (1- (array-dimension matriz 0)) ))
+    (indices matriz)
+    (inicializa-tabla clases)
+    (dbscan matriz eps mu)
+    )
 );defun
