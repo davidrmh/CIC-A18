b72ea7d88db97051395c092ae6c2c7348dcdf636
David <davidricardo888@hotmail.com>
2018-04-13 12:33:56 -0500

    Add mancala

---
+++ IA/mancala/mancala.lisp
@@ -0,0 +1,8 @@
+;;===============================================================
+;; Tablero
+;; Se representa con una lista de listas
+;; cada lista representa una casilla del tablero
+;; Las listas en los índices 6 y 13 representan las bases del jugador y
+;; de la computadora respectivamente
+;;================================================================
+(defvar *tablero* '((r a v) (r a v) (r a v) (r a v) (r a v) (r a v) () (r a v) (r a v) (r a v) (r a v) (r a v) (r a v) ()))
