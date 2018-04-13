ff4e88e3fe80bc99260789918ae7ca611e2ded02
David <davidricardo888@hotmail.com>
2018-04-13 18:35:31 -0500

    Modifies despliega-tablero

--- IA/mancala/mancala.lisp
+++ IA/mancala/mancala.lisp
@@ -23,18 +23,40 @@
 (defparameter *turno* 1)
 
 ;;================================================================
+;; Calcula puntaje
+;;================================================================
+(defun puntaje (lista)
+  "
+  Calcula el puntaje de una casilla en el tablero
+  Regresa una lista con:
+  first: Cadena representando número de elementos de cada tipo
+  second: Puntaje total
+  "
+  (let ((rojo 0) (verde 0) (amarillo 0) (lista-resultado nil) )
+    (loop for elem in lista do
+      (case elem
+        (r (incf rojo) )
+        (v (incf verde) )
+        (a (incf amarillo) ) )    )
+    (setq lista-resultado (list (concatenate 'string "R:" (write-to-string rojo) " A:" (write-to-string amarillo) " V:" (write-to-string verde)  )
+      (reduce #'+ (list (* 10 rojo) (* 5 verde)  (* 1 amarillo) )   )) )
+    lista-resultado
+  );let
+);defun
+
+;;================================================================
 ;; Despliega el tablero en la terminal
 ;;================================================================
 (defun despliega-tablero ()
   (format t "Base-PC     Casilla-12     Casilla-11     Casilla-10     Casilla-9     Casilla-8     Casilla-7")
-  (format t  "~%~a              ~a               ~a               ~a            ~a              ~a            ~a"
-    (length (nth 13 *tablero*)) (length (nth 12 *tablero*)) (length (nth 11 *tablero*)) (length (nth 10 *tablero*)) (length (nth 9 *tablero*))
-  (length (nth 8 *tablero*))  (length (nth 7 *tablero*))  )
+  (format t  "~%~a           ~a    ~a    ~a    ~a   ~a   ~a"
+    (second (puntaje (nth 13 *tablero*))) (first (puntaje (nth 12 *tablero*))) (first (puntaje (nth 11 *tablero*))) (first (puntaje (nth 10 *tablero*))) (first (puntaje (nth 9 *tablero*)))
+  (first (puntaje  (nth 8 *tablero*)))  (first  (puntaje (nth 7 *tablero*)))  )
   (format t "~%==================================================================================================")
-  (format t  "~%~a              ~a               ~a               ~a            ~a              ~a            ~a"
-    (length (nth 0 *tablero*)) (length (nth 1 *tablero*)) (length (nth 2 *tablero*)) (length (nth 3 *tablero*)) (length (nth 4 *tablero*))
-  (length (nth 5 *tablero*))  (length (nth 6 *tablero*))  )
-  (format t  "~%Casilla-0   Casilla-1       Casilla-2       Casilla-3   Casilla-4       Casilla-5   Base-humano")
+  (format t  "~%~a  ~a   ~a    ~a    ~a   ~a   ~a"
+    (first (puntaje (nth 0 *tablero*))) (first (puntaje (nth 1 *tablero*))) (first (puntaje (nth 2 *tablero*))) (first (puntaje (nth 3 *tablero*))) (first (puntaje (nth 4 *tablero*)))
+  (first (puntaje  (nth 5 *tablero*)))  (second  (puntaje (nth 6 *tablero*)))  )
+  (format t  "~%Casilla-0    Casilla-1     Casilla-2      Casilla-3      Casilla-4     Casilla-5     Base-humano")
 );defun
 
 ;;================================================================
