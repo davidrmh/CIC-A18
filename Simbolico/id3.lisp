2fa00a7b95cbd1df791f6ca893771e1974c57654
David <davidricardo888@hotmail.com>
2018-04-16 14:18:39 -0500

    Add split-data

--- Simbolico/id3.lisp
+++ Simbolico/id3.lisp
@@ -45,8 +45,8 @@
     (setq ncol (length (first tabla))) ;Número de atributos
 
     ;Inicializa conteo-etiquetas (lista)
-    ;Conteo etiquetas es una lista cuya i-ésima entrada corresponde a
-    ;el conteo de la i-ésima etiqueta en lista-etiquetas
+    ;Conteo-etiquetas es una lista cuya i-ésima entrada corresponde a
+    ;el conteo de la i-ésima entrada en lista-etiquetas
     (loop for i in lista-etiquetas do (push 0 conteo-etiquetas))
 
     ;Cuenta el número de veces que se presenta una etiqueta
@@ -59,6 +59,45 @@
     (loop for conteo in conteo-etiquetas do
       (setq probabilidad (/ conteo nreng))
       (setq entropia (- entropia (* probabilidad (log probabilidad 2))))  )
-    entropia  
+    entropia
+  );let
+);defun
+
+;;=========================================================
+;; Función para separar el conjunto de datos de acuerdo a
+;; el valor de un atributo.
+;;=========================================================
+(defun split-data (tabla indice valor)
+  "
+  ENTRADA
+  tabla: tabla de datos
+  indice: indice del atributo
+  valor: valor del atributo
+  SALIDA
+  nueva-tabla: nueva tabla omitiendo las observaciones en las cuales el
+  atributo[indice] == valor
+  "
+  (let ((nueva-tabla nil) (ncol 0) (nuevo-renglon nil) )
+    ;Número de columnas (atributos)
+    (setq ncol (length (first tabla)))
+
+    ;comienza a filtrar la información
+    (loop for renglon in tabla do
+
+       (when (equal (nth indice renglon) valor )
+
+        (loop for i from 0 to (1- ncol) do
+            ;Extrae el nuevo renglón omitiendo el índice del atributo dado
+           (if (/= i indice) (setq nuevo-renglon (append nuevo-renglon (list (nth i renglon) ))) ) )
+
+        ;agrega el nuevo renglón a la tabla
+        (push nuevo-renglon nueva-tabla)
+      );when
+
+      ;reinicia nuevo-renglon para nueva iteración
+      (setq nuevo-renglon nil)
+    );loop renglon
+    (setq nueva-tabla (reverse nueva-tabla))
+  nueva-tabla
   );let
 );defun
