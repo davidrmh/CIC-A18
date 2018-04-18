51152636949fe28aeaeb3ea2232e52a05b162c9d
David Montalvan <davidricardo888@hotmail.com>
2018-04-18 00:50:33 -0500

    A lot of changes, but basically were in order to implement abnegamax

--- IA/mancala/mancala.lisp
+++ IA/mancala/mancala.lisp
@@ -27,7 +27,7 @@
 ;;================================================================
 ;; profundidad máxima
 ;;================================================================
-(defparameter *profundidad-max* 10)
+(defparameter *profundidad-max* 100)
 
 ;;================================================================
 ;; Función para inicializar las variables globales
@@ -80,22 +80,25 @@
 ;;================================================================
 ;; Aplica la jugada sobre el tablero
 ;;================================================================
-(defun aplica-jugada (indice-casilla)
+(defun aplica-jugada (indice-casilla tablero-orig turno)
   "
   Modifica el tablero repartiendo las fichas en la casilla índice-casilla
   en las casillas vecinas
+  Regresa una lista con
+  first: nuevo tablero
+  second: booleano para saber si se repetirá de turno
   "
-  (let ((contenido nil) (aux 0)  (tablero nil) (ultima-casilla 0) )
-    (setq tablero (copy-seq *tablero*))
+  (let ((contenido nil) (aux 0)  (tablero nil) (ultima-casilla 0) (bool-repite nil)  )
+    (setq tablero (copy-seq tablero-orig))
     (setq contenido (copy-seq (nth indice-casilla tablero))) ;Copia el contenido de la casilla
     (setq aux (+ indice-casilla 1)) ;Primer casilla vecina
 
     ;Actualiza las  casillas vecinas
     (loop for elem in contenido do
       ;Omite casilla base del jugador humano si fue un movimiento de la máquina
-      (if (and (= aux 6) (= *turno* 2)) (setq aux (mod (1+ aux) 13 )) )
+      (if (and (= aux 6) (= turno 2)) (setq aux (mod (1+ aux) 13 )) )
       ;Omite casilla base de la máquina si fue un movimiento del humano
-      (if (and (= aux 13) (= *turno* 1)) (setq aux (- (mod (1+ aux) 13 ) 1)) )
+      (if (and (= aux 13) (= turno 1)) (setq aux (- (mod (1+ aux) 13 ) 1)) )
 
       ;Actualiza tablero
       (setf (nth aux tablero) (append (nth aux tablero) (list elem)) )
@@ -104,11 +107,11 @@
     );loop
 
     ;Revisa si se va a repetir turno
-    (if (or (and (= ultima-casilla 6) (= *turno* 1) ) (and (= ultima-casilla 13) (= *turno* 2) ) ) (setq *bool-repite* t) (setq *bool-repite* nil)  )
+    (if (or (and (= ultima-casilla 6) (= turno 1) ) (and (= ultima-casilla 13) (= turno 2) ) ) (setq bool-repite t) (setq bool-repite nil)  )
 
     ;Vacía la casilla de movimiento
     (setf (nth indice-casilla tablero) nil)
-    tablero
+    (list tablero bool-repite)
   );let
 );defun
 
@@ -116,12 +119,12 @@
 ;;=============================================================================
 ;; Determina turno
 ;;=============================================================================
-(defun determina-turno (turno-anterior)
+(defun determina-turno (turno-anterior bool-repite)
   (cond
-    ((and (= turno-anterior 1) (equal *bool-repite* t) ) (setq *turno* 1) )
-    ((and (= turno-anterior 2) (equal *bool-repite* t) ) (setq *turno* 2) )
-    ((and (= turno-anterior 1) (equal *bool-repite* nil) ) (setq *turno* 2) )
-    ((and (= turno-anterior 2) (equal *bool-repite* nil) ) (setq *turno* 1) ) )
+    ((and (= turno-anterior 1) (equal bool-repite t) ) (return-from determina-turno 1) )
+    ((and (= turno-anterior 2) (equal bool-repite t) ) (return-from determina-turno 2) )
+    ((and (= turno-anterior 1) (equal bool-repite nil) ) (return-from determina-turno 2) )
+    ((and (= turno-anterior 2) (equal bool-repite nil) ) (return-from determina-turno 1) ) )
 );defun
 
 ;;=============================================================================
@@ -140,7 +143,7 @@
 ;; 2. Que la casilla no sea nil (al menos tiene una ficha)
 ;; 3. El índice de la casilla seleccionada no sea una casilla del oponente
 ;;============================================================================
-(defun valida-jugada (indice-casilla tablero)
+(defun valida-jugada (indice-casilla tablero turno)
   "
   Valida una jugada POR HACER
   indice-casilla: índice de la casilla que se desea jugar
@@ -154,9 +157,91 @@
     (if (not (equal (nth indice-casilla tablero) nil)) (setq prueba2 t) (return-from valida-jugada nil)  )
 
     ;prueba 3
-    (if (or (and (>= indice-casilla 0) (<= indice-casilla 5) (= *turno* 1) )
-      (and (>= indice-casilla 7) (<= indice-casilla 12) (= *turno* 2) ) ) (setq prueba3 t) (return-from valida-jugada nil) )
+    (if (or (and (>= indice-casilla 0) (<= indice-casilla 5) (= turno 1) )
+      (and (>= indice-casilla 7) (<= indice-casilla 12) (= turno 2) ) ) (setq prueba3 t) (return-from valida-jugada nil) )
     (if (and prueba1 prueba2 prueba3) t nil)
+  );let
+);defun
+
+;;===========================================================================
+;; Función para evaluar el tablero
+;; da prioridad a los movimientos que permiten tener turnos consecutivos
+;; si no hay casillas que proporcionen una repetición en el turno
+;; se le asigna un número aleatorio :p
+;;===========================================================================
+(defun evalua-tablero (tablero)
+  (if (es-terminal? tablero) (return-from evalua-tablero 100) )
+  (cond
+    ((= (second (puntaje (nth 0 tablero))) 6)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 1 tablero))) 5)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 2 tablero))) 4)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 3 tablero))) 3)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 4 tablero))) 2)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 5 tablero))) 1)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 12 tablero))) 1)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 11 tablero))) 2)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 10 tablero))) 3)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 9 tablero))) 4)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 8 tablero))) 5)  (return-from evalua-tablero 10)  )
+    ((= (second (puntaje (nth 7 tablero))) 6)  (return-from evalua-tablero 10)  )
+    (t (return-from evalua-tablero (1+ (random 9) ) ) )
+  )
+);defun
+;;============================================================================
+;; Negamax
+;;============================================================================
+(defun abnegamax (tablero profundidad turno alfa beta  &optional (profundidad-max *profundidad-max*) )
+  "
+  Algoritmo negamax
+  regresa una lista con dos elementos
+  first: mejor-valor
+  second: mejor-movimiento
+  "
+  (let ((lista-resultado nil) (mejor-movimiento nil) (mejor-puntuacion nil) (nuevo-tablero nil)
+        (puntaje-actual nil) (lista-movimientos nil)
+        (lista-aux nil) (nuevo-turno nil) (bool-repite nil) )
+
+        ;Revisa si es estado terminal o se llegó a la profundidad máxima
+        (when (or (es-terminal? tablero) (>= profundidad profundidad-max)  )
+          (setq lista-resultado (list (evalua-tablero tablero) nil) )
+          (return-from abnegamax lista-resultado) )
+
+        ;(format t "Profundidad = ~a ~%" profundidad)
+        ;(sleep 0.5)
+        (setq mejor-movimiento  nil)
+        (setq mejor-puntuacion -1000000000)
+
+        ;Obtiene los movimientos permitidos en el estado actual del tablero
+        (loop for i in '(0 1 2 3 4 5 7 8 9 10 11 12) do
+          (if (valida-jugada i tablero turno)
+            (setq lista-movimientos (append lista-movimientos (list i))) ) )
+
+        (loop for movimiento in lista-movimientos do
+          (setq lista-aux (aplica-jugada movimiento tablero turno))
+          (setq nuevo-tablero (first lista-aux))
+          (setq bool-repite (second lista-aux))
+          (setq nuevo-turno (determina-turno turno bool-repite))
+
+          (setq lista-resultado (abnegamax nuevo-tablero (1+ profundidad) nuevo-turno (* -1 beta) (* -1 (max alfa mejor-puntuacion))  ))
+
+          ;Considera la repetición de turnos
+          ;si no repite se multiplica por -1
+          (setq puntaje-actual (first lista-resultado))
+          (if  (not bool-repite) (setq puntaje-actual (* -1 puntaje-actual)) )
+
+          ;actualiza puntajes
+          (when (> puntaje-actual mejor-puntuacion)
+            (setq mejor-puntuacion puntaje-actual)
+            (setq mejor-movimiento movimiento)
+
+            ;Revisa la condición alfa-beta
+            (when (>= mejor-puntuacion beta) (setq lista-resultado (list mejor-puntuacion mejor-movimiento))
+              (return-from abnegamax lista-resultado)) )
+
+        ) ;loop
+
+      (setq lista-resultado (list mejor-puntuacion mejor-movimiento))
+      lista-resultado
   );let
 );defun
 
@@ -164,7 +249,8 @@
 ;; inicia el juego
 ;;============================================================================
 (defun main (&optional (jugadores 2))
-  (let ((jugador1 nil) (jugador2 nil) (puntaje1 0) (puntaje2 0) )
+  (let ((jugador1 nil) (jugador2 nil) (puntaje1 0) (puntaje2 0) (lista-aux nil) )
+    (inicializa)
     (setq *turno* 1)
     (despliega-tablero)
 
@@ -175,10 +261,12 @@
       (format t "~% Elige la casilla ")
         (loop
            (setq jugador1 (read))
-           (if (not (valida-jugada jugador1 *tablero*)) (format t "~%Jugada no válida, elige otra opción "))
-           (when (valida-jugada jugador1 *tablero*) (return t)) );loop
+           (if (not (valida-jugada jugador1 *tablero* *turno*)) (format t "~%Jugada no válida, elige otra opción "))
+           (when (valida-jugada jugador1 *tablero* *turno*) (return t)) );loop
 
-        (setq *tablero* (aplica-jugada jugador1)) ;aplica la jugada
+        (setq lista-aux (aplica-jugada jugador1 *tablero* *turno*))
+        (setq *tablero* (first lista-aux)) ;aplica la jugada
+        (setq *bool-repite* (second lista-aux))
         (despliega-tablero)
         (format t "~%El jugador ~a modificó la casilla ~a ~%" *turno* jugador1)
         (if *bool-repite* (format t "~%Jugador ~a vuelve a jugar~%" *turno*))
@@ -190,10 +278,12 @@
       (format t "~% Elige la casilla ")
         (loop
            (setq jugador2 (read))
-           (if (not (valida-jugada jugador2 *tablero*)) (format t "~%Jugada no válida, elige otra opción "))
-           (when (valida-jugada jugador2 *tablero*) (return t) ) );loop
+           (if (not (valida-jugada jugador2 *tablero* *turno*)) (format t "~%Jugada no válida, elige otra opción "))
+           (when (valida-jugada jugador2 *tablero* *turno*) (return t) ) );loop
 
-        (setq *tablero* (aplica-jugada jugador2)) ;aplica jugada
+        (setq lista-aux (aplica-jugada jugador2 *tablero* *turno*))
+        (setq *tablero* (first lista-aux)) ;aplica jugada
+        (setq *bool-repite* (second lista-aux))
         (despliega-tablero)
         (format t "~%El jugador ~a modificó la casilla ~a ~%" *turno* jugador2)
         (if *bool-repite* (format t "~%Jugador ~a vuelve a jugar~%" *turno*))
