9da8b88ed8694ab88d08724452396af54d9f5426
David <davidricardo888@hotmail.com>
2018-05-23 14:34:10 -0500

    Add flare.data2 and estrella.lisp

---
+++ Simbolico/estrella.lisp
@@ -0,0 +1,41 @@
+;;==============================================================================
+;; Función para leer los datos
+;; ENTRADA
+;; nombre-archivo. String. Ruta de los datos
+;;
+;; SALIDA
+;; datos. Lista. Una lista con cada elemento representando una línea del archivo
+;;
+;; ESTRUCTURA DEL ARCHIVO
+;; El archivo debe de tener la siguiente estructura:
+;; (val11 val12 val13...)
+;; (val21 val22 val23...)
+;;==============================================================================
+(defun lee-datos (nombre-archivo)
+  (let ((archivo nil) (datos nil) )
+    (setq archivo (open nombre-archivo))
+    (loop for linea = (read archivo nil nil) while linea do
+      (setq datos (append datos (list linea)))
+    );loop
+    (close archivo)
+    datos);let
+);defun
+
+;;==============================================================================
+;; Función para obtener una lista con las clases posibles (sin repeticiones)
+;;
+;; ENTRADA
+;; datos: Lista. Lista creada con la función lee-datos
+;; indice: Entero. Índice (iniciando en 0) de la columna que tiene la clase
+;;
+;; SALIDA
+;; clases: Lista. Lista con las clases en el conjunto de datos
+;;==============================================================================
+(defun obten-clases (datos indice)
+  (let ((clases nil) (clase-aux nil))
+    (loop for observacion in datos do
+      (setq clase-aux (nth indice observacion))
+      (if (not (find clase-aux clases)) (setq clases (append clases  (list clase-aux)))) );loop
+  clases      
+  );let
+);defun
