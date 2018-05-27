2d307ab6375076a990669224658bbb988aaa757b
David Montalvan <davidricardo888@hotmail.com>
2018-05-27 18:49:41 -0500

    Add decision tree

--- patrones/articulo/experimentos.py
+++ patrones/articulo/experimentos.py
@@ -5,6 +5,7 @@ import copy as cp
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn import svm
+from sklearn import tree
 ##==============================================================================
 ##                       EXPERIMENTOS PARA EL ARTÍCULO
 ##==============================================================================
@@ -41,3 +42,30 @@ def clasificadorSVM(entrenamiento,prueba):
     prueba['Clase']=modelo.predict(pruebaNormalizados)
 
     return prueba
+
+##==============================================================================
+##                          ÁRBOL DE DECISIÓN C4.5
+##==============================================================================
+def clasificadorArbol(entrenamiento,prueba):
+        '''
+        ENTRADA
+        entrenamiento: Pandas DataFrame. Conjunto de entrenamiento con últimas
+        tres columnas Clase, Date y Adj Close
+
+        prueba: Pandas DataFrame. Conjunto de prueba con últimas dos columnas
+        Date y Adj Close
+
+        SALIDA
+        prueba: Pandas DataFrame. Conjunto de prueba con la nueva columna Clase
+        '''
+        #Atributos utilizados para ajustar el modelo
+        numAtributos=entrenamiento.shape[1]-3
+
+        #Ajusta modelo
+        modelo=tree.DecisionTreeClassifier(random_state=0,max_depth=3)
+        modelo.fit(entrenamiento.iloc[:,0:numAtributos],entrenamiento['Clase'])
+
+        #Realiza las predicciones
+        prueba['Clase']=modelo.predict(prueba.iloc[:,0:numAtributos])
+
+        return prueba
